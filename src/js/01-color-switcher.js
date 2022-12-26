2ac4736bdc556cc2de18cbe0cb0cf7084acf96a6
OksanaTur <ksyturkevych@gmail.com>
2022-12-26 16:08:38 +0400

    add files

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
 
+buttonStop.disabled = true;
+// console.log(buttonStop)
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStart.addEventListener('click', () => {
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+
+    }, 1000);
+});
+
+buttonStop.addEventListener('click', () =>
+{
+    clearInterval(timerId);
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+})
